666b26dde7452dacca84a7bbca368cc6e6e610b9
Tony An <anxuanzi@gmail.com>
2023-12-27 13:58:29 -0800

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "creating portainer container..."
+
 docker volume create portainer_data
 
 docker run -d \
@@ -12,6 +14,23 @@ docker run -d \
   --label com.centurylinklabs.watchtower.enable=true \
   portainer/portainer-ce:latest
 
+# File path
+DOCKER_CONFIG_FILE="/root/.docker/config.json"
+
+# Check if the file does not exist
+if [ ! -f "$DOCKER_CONFIG_FILE" ]; then
+    # Create the directory if it doesn't exist
+    mkdir -p /root/.docker
+
+    # Create the file
+    touch "$DOCKER_CONFIG_FILE"
+    echo "Docker Configuration File created: $DOCKER_CONFIG_FILE"
+else
+    echo "File already exists: $DOCKER_CONFIG_FILE"
+fi
+
+echo "setting up watchertower container..."
+
 docker run -d \
   --name watchtower \
   --restart always \
@@ -20,6 +39,6 @@ docker run -d \
   -e WATCHTOWER_CLEANUP=true \
   -e WATCHTOWER_INCLUDE_STOPPED=true \
   -e WATCHTOWER_REVIVE_STOPPED=true \
-  -e WATCHTOWER_POLL_INTERVAL=10 \
+  -e WATCHTOWER_POLL_INTERVAL=5 \
   -e WATCHTOWER_LABEL_ENABLE=true \
   containrrr/watchtower:latest
